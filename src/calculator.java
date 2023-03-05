524f25e540b3bf85d74a8b2549175b19768babae
Nikmet <metlov.n@mail.ru>
2023-03-05 15:51:47 +0300

    all2

--- src/calculator.java
+++ src/calculator.java
@@ -2,27 +2,25 @@ import java.util.Scanner;
 
 public class calculator {
 
-    public static float calculate(char operator, int n1, int n2) {
-        switch (operator) {
-            case '+' -> {
-                return (float) (n1 + n2);
-            }
-            case '-' -> {
-                return (float) (n1 - n2);
-            }
-            case '*' -> {
-                return (float) (n1 * n2);
-            }
-            case '/' -> {
-                return (float) (n1 / n2);
-            }
-            case '^' -> {
-                return (float) Math.pow((float) n1, (float) n2);
-            }
-            default -> System.out.println("Вы ввели не допустимое значение!");
-
-        }
-        return 0;
+    public static double sum(double n1, double n2) {
+        return n1 + n2;
     }
+
+    public static double sub(double n1, double n2) {
+        return n1 - n2;
+    }
+
+    public static double mul(double n1, double n2) {
+        return n1 * n2;
+    }
+
+    public static double div(double n1, double n2) {
+        return n1 / n2;
+    }
+
+    public static double pow(double n1, double n2) {
+        return Math.pow(n1, n2);
+    }
+
 
 }
--- src/calculatorTEST.java
+++ src/calculatorTEST.java
@@ -4,26 +4,26 @@ import org.junit.jupiter.api.Test;
 public class calculatorTEST {
     @Test
     public void sumTest(){
-        Assertions.assertEquals(calculator.calculate('+', 15, 14), 29);
+        Assertions.assertEquals(calculator.sum(15, 14), 29);
     }
 
     @Test
     public void subTest(){
-        Assertions.assertEquals(calculator.calculate('-', 15, 14), 1);
+        Assertions.assertEquals(calculator.sub(15, 14), 1);
     }
 
     @Test
     public void mulTest(){
-        Assertions.assertEquals(calculator.calculate('*', 15, 2), 30);
+        Assertions.assertEquals(calculator.mul(15, 2), 30);
     }
 
     @Test
     public void divTest(){
-        Assertions.assertEquals(calculator.calculate('/', 15, 5), 3);
+        Assertions.assertEquals(calculator.div(15, 5), 3);
     }
 
     @Test
     public void powTest(){
-        Assertions.assertEquals(calculator.calculate('^', 2, 5), 32);
+        Assertions.assertEquals(calculator.pow(2, 5), 32);
     }
 }
